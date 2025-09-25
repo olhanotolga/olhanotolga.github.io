5fa86aaaeb7de7162d373430a9023e6f1ea8c276
olhanotolga <olha.halat@gmail.com>
2025-09-25 19:19:28 +0200

    feat: stop hiding and showing Home button

--- script/main.js
+++ script/main.js
@@ -1,35 +1,4 @@
 /*
- *** Logic to show/hide the arrow up (Home) button ***
- */
-
-const arrow = document.getElementById('up');
-const scrolling = () => {
-  let yPosInit = window.scrollY;
-  let yPosNext;
-  setTimeout(() => {
-    yPosNext = window.scrollY;
-
-    if (yPosInit > 0 && yPosInit >= yPosNext) {
-      arrow.classList.remove('hide');
-      arrow.classList.add('show');
-    } else {
-      arrow.classList.remove('show');
-      arrow.classList.add('hide');
-    }
-  }, 300);
-
-  if (yPosInit > 0 && yPosInit > yPosNext) {
-    arrow.classList.remove('hide');
-    arrow.classList.add('show');
-  } else {
-    arrow.classList.remove('show');
-    arrow.classList.add('hide');
-  }
-};
-
-window.addEventListener('scroll', scrolling);
-
-/*
  *** Logic to navigate in the Projects Grid ***
  */
 
